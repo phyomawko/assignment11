0ee9c8ac0a4e130638b5a831c935a11efdf4c958
phyomawko <52078345+phyomawko@users.noreply.github.com>
2025-03-06 14:57:16 +0630

    remove key pari

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "app_server" {
   ami           =  var.ami
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.application_subnet.id
-  key_name      = "pmk-own-labs"
+  # key_name      = "pmk-own-labs"
   vpc_security_group_ids = [aws_security_group.web_sg.id]
   
 
